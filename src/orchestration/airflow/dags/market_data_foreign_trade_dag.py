fba7d36ae487de990d0cc3b1408b0fb3d9bb4a61
tuong nguyen <tuongnguyen@tuongs-Mac-mini.local>
2026-03-28 15:43:57 +0700

    Done refactor

--- src/orchestration/airflow/dags/market_data_foreign_trade_dag.py
+++ src/orchestration/airflow/dags/market_data_foreign_trade_dag.py
@@ -1,62 +1,21 @@
-#query connection from ariflow connection
-
-from airflow.hooks.base import BaseHook
 from airflow import DAG
 from datetime import datetime
 from airflow.operators.python import PythonOperator
-from src.infrastructure.polars.etl.extract.postgre_db import PostgreDBExtractorWithPolars
-from google.oauth2 import service_account
-import json
 
-from src.infrastructure.polars.etl.load.bq_loader import BigQueryLoaderPolars
-from src.jobs.silver_market_data import SilverMarketData
+from src.orchestration.airflow.python_script.postgre_to_bq import main
 
-# Deffine extractor
-polars_connection = BaseHook.get_connection('postgres_market_data_polar_uri')
-uri = polars_connection.password
-query = """
-    SELECT id::text AS id,
-        stock_id,
-        trading_date,
-        foreign_room,
-        buy_volume,
-        sell_volume
-    FROM public.foreign_trade
- """
-
-extractor = PostgreDBExtractorWithPolars(
-    uri=uri,
-    query=query,
-)
-
-# Define loader
-bq_connectyion = BaseHook.get_connection('gcp_sa_for_bq_data_append')
-json_credentials = json.loads(bq_connectyion.password)
-credentials = service_account.Credentials.from_service_account_info(json_credentials)
-
-loader = BigQueryLoaderPolars(
-    gcp_credential=credentials,
-    project="rich-finance-2026",
-    dataset="market_data",
-    table="foreign_trade",
-)
-
-silver_market_data_jobs = SilverMarketData(
-    extractor=extractor,
-    loader=loader,
-)
-
-def run_silver_market_data_jobs():
-    silver_market_data_jobs.run()
+JOB_CONFIG_PATH = "/opt/airflow/dags/etl_pipeline_dags/src/config/postgre_to_bq/market_data/foreign_trade.yaml"
 
 with DAG(
     dag_id='market_data_foreign_trade_dag',
     start_date=datetime(2024, 1, 1),
     schedule=None,
-    catchup=False,
-    tags=['market_data', 'foreign_trade'],
+    catchup=False
 ) as dag:
     task1 = PythonOperator(
-        task_id='run_silver_market_data_jobs',
-        python_callable=run_silver_market_data_jobs
+        task_id='run_silver_market_data_foreign_trade_jobs',
+        python_callable=main,
+        op_kwargs={
+            "job_config_path": JOB_CONFIG_PATH
+        },
     )
