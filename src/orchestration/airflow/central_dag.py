954ed07bad9dad343ba9d2900ee17d409f76e0a2
tuong nguyen <tuongnguyen@tuongs-Mac-mini.local>
2026-04-04 14:01:35 +0700

    done factory pattern

--- src/orchestration/airflow/central_dag.py
+++ src/orchestration/airflow/central_dag.py
@@ -1,5 +1,7 @@
 from src.utils.airflow_config_loader import load_config
 from typing import Any
+from pathlib import Path
+from src.utils import airflow_config_loader
 from src.orchestration.config.config_registry import ConfigRegistry
 from src.orchestration.dag_builder.task_factories.task_factory_registry import TaskFactoryRegistry
 from src.orchestration.dag_builder.task_factories.dummy_factory import DummyTaskFactory
@@ -7,7 +9,7 @@ from src.orchestration.dag_builder.dag_builder import DagBuilder
 
 
 
-configs_path = "local://src/configs/orchestration"
+configs_path = f"local://{Path(airflow_config_loader.__file__).resolve().parents[1] / 'configs' / 'orchestration'}"
 raw_configs: list[dict[str, Any]] = load_config(path=configs_path)
 config_registry = ConfigRegistry()
 config_registry.populate(raw_configs)
