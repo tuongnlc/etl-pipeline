d80690be1bb6577d9e154acdfa1b077db1969b65
tuong nguyen <tuongnguyen@tuongs-Mac-mini.local>
2026-04-07 20:34:41 +0700

    add first test for models

---
+++ test/unit/models/conftest.py
@@ -0,0 +1,17 @@
+# conftest.py
+import pytest
+from src.models.etl.jobs.postgre_to_bq_silver import PostgreToBqSilverConfig
+
+
+@pytest.fixture(params=[
+    (PostgreToBqSilverConfig, {"loader": "test_loader", "extractor": "test_extractor"}),
+    # (OtherConfig, {"param1": "value1", "param2": "value2"})
+])
+def config_instance(request):
+    config_class, kwargs = request.param
+    return config_class(**kwargs)
+
+def test_all_configs(config_instance):
+    # Test common behavior for all configs
+    assert config_instance is not None
+    # Add more generic assertions
