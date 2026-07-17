506e1b8cb1f505a042866721be33fb740d157488
tuong nguyen <tuongnguyen@tuongs-Mac-mini.local>
2026-07-17 15:19:21 +0700

    done bronze stock_price

--- etl_pipeline/orchestration/airflow/python_script/postgre_to_qdrant_bronze.py
+++ etl_pipeline/orchestration/airflow/python_script/postgre_to_qdrant_bronze.py
@@ -45,11 +45,6 @@ def main(
     else:
         execution_date_filter = None
 
-    print("filter_type: ", job_config.extractor.filter_type)
-
-    print("execution_date: ", execution_date)
-    print("execution_date_filter: ", execution_date_filter)
-
     extractor = PostgreDBExtractorWithPolars(
         source_table_name=args.job_config.extractor.source_table_name,
         uri=uri,
